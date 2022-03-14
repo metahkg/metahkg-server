754e986559f5170cf15df9858f144083468b10fb
wcyat <sleep@wcyat.me>
2022-03-14 23:07:47 +0800

    use regex for search

--- src/router/search.ts
+++ src/router/search.ts
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 import { mongouri } from "../common";
 import isInteger from "is-sn-integer";
 const router = express.Router();
-/*
+/**
  * sort:
  * 0: by relevence //default
  * 1: by creation time
@@ -13,7 +13,8 @@ router.get("/api/search", async (req, res) => {
   if (
     !req.query.q ||
     (req.query.sort && ![0, 1, 2].includes(Number(req.query.sort))) ||
-    (req.query.page && !isInteger(String(req.query.page)))
+    (req.query.page && !isInteger(String(req.query.page))) ||
+    (req.query.mode && ![0, 1].includes(Number(req.query.mode)))
   ) {
     res.status(400);
     res.send({ error: "Bad request." });
@@ -21,16 +22,21 @@ router.get("/api/search", async (req, res) => {
   }
   const client = new MongoClient(mongouri);
   const page = Number(req.query.page) || 1;
+  const q = decodeURIComponent(String(req.query.q));
   try {
     await client.connect();
     const summary = client.db("metahkg-threads").collection("summary");
-    const sort:any = {
+    const sort: any = {
       0: {},
       1: { createdAt: -1 },
       2: { lastModified: -1 },
     }[Number(req.query.sort ?? 0)];
+    const find: any = {
+      0: { title: new RegExp(q, "i") },
+      1: { op: new RegExp(q, "i") }
+    }[Number(req.query.mode ?? 0)];
     const data = await summary
-      .find({ $text: { $search: req.query.q } })
+      .find(find)
       .sort(sort)
       .skip(25 * (page - 1))
       .limit(25)
