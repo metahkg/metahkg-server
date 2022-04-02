2cb7957da545daf8f14acd8d71cdabfff07776c2
wcyat <sleep@wcyat.me>
2022-04-02 20:57:42 +0800

    update username / sex in threads also

--- src/router/account/editprofile.ts
+++ src/router/account/editprofile.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import body_parser from "body-parser";
 import { Type } from "@sinclair/typebox";
 import { ajv } from "../lib/ajv";
-import { getuser, signedin } from "../lib/users";
+import { getuser } from "../lib/users";
 import { error400 } from "../lib/errors/400";
 import { error404 } from "../lib/errors/404";
 import { client } from "../../common";
@@ -10,7 +10,15 @@ const router = Router();
 router.post(
   "/api/account/editprofile",
   body_parser.json(),
-  async (req, res) => {
+  async (req: {
+    body: {
+      user?: string;
+      sex?: "M" | "F";
+    },
+    cookies: {
+      key?: string;
+    }
+  }, res) => {
     const schema = Type.Object(
       {
         user: Type.Optional(Type.String()),
@@ -28,10 +36,28 @@ router.post(
       return;
     }
     const users = client.db("metahkg-users").collection("users");
+    const limit = client.db("metahkg-users").collection("limit");
     const summary = client.db("metahkg-threads").collection("summary");
+    const threadusers = client.db("metahkg-threads").collection("users");
     await summary.updateMany(
       { op: user.user },
       { $set: { op: req.body.user, sex: req.body.sex } }
+    );
+    await threadusers.updateMany(
+      {
+        [user.id]: {
+          name: user.user,
+          sex: user.sex,
+        },
+      },
+      {
+        $set: {
+          [user.id]: {
+            name: req.body.user || user.user,
+            sex: req.body.sex || user.sex,
+          },
+        },
+      }
     );
     await users.updateOne({ key: req.cookies.key }, { $set: req.body });
     res.send({ response: "ok" });
