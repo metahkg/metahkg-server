7d780c97cb95fb820ffeaa6d43f7dd898e460d6a
wcyat <sleep@wcyat.me>
2022-04-13 13:33:07 +0800

    jwt exceptions

--- src/router/auth/verify.ts
+++ src/router/auth/verify.ts
@@ -5,8 +5,12 @@ import jwt_decode from "jwt-decode";
 
 export default function verifyUser(token: string): false | jwtTokenType {
     if (!token) return false;
-    const key = process.env.jwtKey;
-    const data = jwt.verify(token, key, { algorithms: ["HS256"] });
-    if (!data || !ajv.validate(jwtTokenSchema, data)) return false;
+    const jwtKey = process.env.jwtKey;
+    try {
+        const data = jwt.verify(token, jwtKey, { algorithms: ["HS256"] });
+        if (!data || !ajv.validate(jwtTokenSchema, data)) return false;
+    } catch {
+        return false;
+    }
     return jwt_decode(token);
 }
