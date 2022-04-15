e0d3663b67b545bbdb6c8999c5740883c13e0cfa
wcyat <sleep@wcyat.me>
2022-04-15 13:18:18 +0800

    use ?.slice

--- src/lib/auth/verify.ts
+++ src/lib/auth/verify.ts
@@ -4,7 +4,7 @@ import { jwtTokenSchema, jwtTokenType } from "../../types/jwt/user";
 import jwt_decode from "jwt-decode";
 
 export default function verifyUser(token: string): false | jwtTokenType {
-    token = token.slice(7);
+    token = token?.slice(7);
     if (!token) return false;
     const jwtKey = process.env.jwtKey;
     try {
