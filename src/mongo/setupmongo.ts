0f259b79a05d8f43f58715c4e9dba5ce9b08331d
wcyat <sleep@wcyat.me>
2022-04-19 15:14:11 +0800

    fix mongo

--- src/mongo/setupmongo.ts
+++ src/mongo/setupmongo.ts
@@ -1,10 +1,10 @@
-import fs from "fs";
 import { viralCl, summaryCl, limitCl, verificationCl, categoryCl } from "../common";
+import { categories } from "./category";
 export async function setup() {
     if ((await categoryCl.find().toArray()).length) {
         console.log("documents found. not setting up again");
     } else {
-        await categoryCl.insertMany(JSON.parse(fs.readFileSync("category.json", "utf8")));
+        await categoryCl.insertMany(categories);
         await viralCl.createIndex({ createdAt: 1 }, { expireAfterSeconds: 172800 });
         await summaryCl.createIndex({ op: "text", title: "text" }); //text search
         await limitCl.createIndex({ createdAt: 1 }, { expireAfterSeconds: 86400 });
