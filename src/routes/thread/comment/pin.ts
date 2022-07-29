032f66372afe6fdd7ab6ee2651f76766e75fe48a
wcyat <wcyat@wcyat.me>
2022-07-30 02:12:13 +0800

    fix pin comment

--- src/routes/thread/comment/pin.ts
+++ src/routes/thread/comment/pin.ts
@@ -41,6 +41,7 @@ export default function (
                 {
                     projection: {
                         _id: 0,
+                        op: 1,
                         conversation: {
                             $elemMatch: {
                                 id: commentId,
@@ -52,14 +53,10 @@ export default function (
 
             if (!thread) return res.code(404).send({ error: "Thread not found." });
 
-            if (thread.op.id !== user.id)
+            if (thread?.op?.id !== user.id)
                 return res.code(403).send({ error: "Forbidden." });
 
-            const comment = Object.fromEntries(
-                Object.entries(thread.conversation?.[0]).filter(
-                    (i) => !["replies", "U", "D"].includes(i[0])
-                )
-            )[0] as commentType;
+            const comment = thread.conversation?.[0] as commentType;
 
             if (!comment) return res.code(404).send({ error: "Comment not found." });
             if (comment.removed) return res.code(410).send({ error: "Comment removed." });
