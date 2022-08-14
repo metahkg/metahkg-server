5fa789a7fd1a3df8bbbdfe3f47271324448c76b2
wcyat <wcyat@wcyat.me>
2022-08-14 12:08:14 +0800

    add migrate scripts

---
+++ src/migrate/v4.2/v4.2.0.ts
@@ -0,0 +1,43 @@
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+import { exit } from "process";
+
+dotenv.config();
+
+async function migrate() {
+    console.log("migrating to v4.2.0...");
+
+    if (!process.env.DB_URI) throw new Error("Missing DB_URI environment variable.");
+
+    const client = new MongoClient(process.env.DB_URI);
+    await client.connect();
+    const db = client.db("metahkg");
+    const usersCl = db.collection("users");
+
+    await Promise.all(
+        (
+            await usersCl.find().toArray()
+        ).map(async (data) => {
+            if (data.blocked) {
+                await usersCl.updateOne(
+                    {
+                        _id: data._id,
+                    },
+                    {
+                        $set: {
+                            blocked: data.blocked.map((i: any) => ({
+                                date: Date.now(),
+                                reason: "",
+                                ...i,
+                            })),
+                        },
+                    }
+                );
+            }
+        })
+    );
+}
+
+migrate().then(() => {
+    exit(0);
+});
