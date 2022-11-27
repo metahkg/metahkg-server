bb50081551b218fc2bde6547c434c6f3afebed8b
wcyat <wcyat@wcyat.me>
2022-11-27 19:01:49 +0800

    migrate scripts: link to v5.7.3 and v5.7.3-2

---
+++ src/migrate/v5.7/v5.7.3.ts
@@ -0,0 +1,58 @@
+/*
+ Copyright (C) 2022-present Metahkg Contributors
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import dotenv from "dotenv";
+import { MongoClient, ObjectId } from "mongodb";
+import { exit } from "process";
+import EmailValidator from "email-validator";
+import { sha256 } from "hash.js";
+
+dotenv.config();
+
+async function migrate() {
+    console.log("migrating to v5.7.2...");
+
+    if (!process.env.DB_URI) throw new Error("Missing DB_URI environment variable.");
+
+    const client = new MongoClient(process.env.DB_URI);
+    await client.connect();
+    const db = client.db("metahkg");
+    const verificationCl = db.collection("verification");
+
+    await Promise.all(
+        (
+            await verificationCl
+                .find({ email: { $exists: true } })
+                .project({ email: 1, _id: 1 })
+                .toArray()
+        ).map(async (doc: { _id: ObjectId; email: string }) => {
+            // if is email = not yet hashed
+            if (EmailValidator.validate(doc.email)) {
+                await verificationCl.updateOne(
+                    { _id: doc._id },
+                    { $set: { email: sha256().update(doc.email).digest("hex") } }
+                );
+            }
+        })
+    );
+
+    await db.collection("limit").drop();
+}
+
+migrate().then(() => {
+    exit(0);
+});
