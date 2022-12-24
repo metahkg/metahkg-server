1974c81e024ca488a8798ef35f52a3ee46db9bbe
wcyat <wcyat@wcyat.me>
2022-12-25 01:15:34 +0800

    fix: forgot: recaptcha

--- src/routes/users/forgot.ts
+++ src/routes/users/forgot.ts
@@ -58,7 +58,7 @@ export default (
             const { email, rtoken } = req.body;
             const hashedEmail = sha256(email);
 
-            if (!verifyCaptcha(RecaptchaSecret, rtoken))
+            if (!(await verifyCaptcha(RecaptchaSecret, rtoken)))
                 return res
                     .code(429)
                     .send({ statusCode: 429, error: "Recaptcha token invalid." });
